eb97e70a45089adb059937cea86a70db62390676
notehg <110551666+notehg@users.noreply.github.com>
2023-09-09 12:27:06 -0300

    Add files via upload

---
+++ tcc-10/detalhe.php
@@ -0,0 +1,39 @@
+<?php
+include 'conexao.php';
+
+// Obtém o ID do livro da URL
+if (isset($_GET['id'])) {
+    $fileId = $_GET['id'];
+
+    // Busca os detalhes do livro no banco de dados usando o ID
+    $sql = "SELECT * FROM files WHERE id = $fileId";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $fileTitle = $row["title"];
+        $fileGenre = $row["genre"];
+        $pdfFilename = $row["pdf_filename"];
+        $imageFilename = $row["image_filename"];
+        $sinopse = $row["sinopse"];
+
+        // Exibe os detalhes do livro de forma organizada
+        echo "<h1>$fileTitle</h1>";
+        echo "<img src='uploads/images/$imageFilename' alt='$fileTitle'><br>";
+        echo "<p><strong>Gênero:</strong> $fileGenre</p>";
+        echo "<p><strong>Sinopse:</strong> $sinopse</p>";
+
+        // Botão para ler online (abre o PDF em uma nova aba)
+        echo "<a href='uploads/$pdfFilename' target='_blank'>Ler Online</a>";
+
+        // Botão para baixar o livro (fornece o link para download)
+        echo "<a href='uploads/$pdfFilename' download>Baixar PDF</a>";
+    } else {
+        echo "Livro não encontrado.";
+    }
+} else {
+    echo "ID do livro não fornecido na URL.";
+}
+
+mysqli_close($conn);
+?>
---
+++ tcc-10/funcoesphp/filtrar.php
@@ -0,0 +1,39 @@
+<?php
+include 'conexao.php';
+
+// Inicializa a variável de filtro de gênero
+$selectedGenre = "";
+
+// Verifica se um gênero foi selecionado a partir do menu dropdown
+if (isset($_GET['genre'])) {
+    $selectedGenre = $_GET['genre'];
+
+    // Filtra os livros com base no gênero selecionado
+    $sql = "SELECT * FROM files WHERE genre LIKE '%$selectedGenre%'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        // Exibe os livros filtrados
+        echo "<h2>Livros do Gênero: $selectedGenre</h2>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $fileId = $row["id"];
+            $fileTitle = $row["title"];
+            $imageFilename = $row["image_filename"];
+    
+            // Exibe a imagem como um link para a página de detalhes
+            echo "<a href='detalhe.php?id=$fileId'>";
+            echo "<img src='uploads/images/$imageFilename' alt='$fileTitle' width='100'>";
+            echo "</a><br>";
+            echo "Título: $fileTitle<br>";
+    
+
+
+        }
+    } else {
+        echo "Nenhum livro encontrado.";
+    }
+}
+
+mysqli_close($conn);
+
+?>
